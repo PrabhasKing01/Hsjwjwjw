cb80f02c0f48876ce14dc6b48154619ad17f2fe8
PrabhasKing01 <132885070+PrabhasKing01@users.noreply.github.com>
2026-02-08 11:43:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+# config.py
+
+API_ID = 1234567  # Apna API ID yahan dalein (integer)
+API_HASH = "your_api_hash_here" # Apna API HASH yahan dalein
+BOT_TOKEN = "your_bot_token_here" # BotFather wala token
+ADMIN_ID = 123456789  # Apna Numerical Telegram ID dalein
+
+# Fancy Start Message
+START_MSG = """
+<b>┏━━━━━━━━━━━━━━━━━━━━┓
+   ✨ 𝕎𝕖𝕝𝕔𝕠𝕞𝕖 𝕋𝕠 𝕆𝕦𝕣 𝔹𝕠𝕥 ✨
+┗━━━━━━━━━━━━━━━━━━━━┛</b>
+
+👋 ʜᴇʟʟᴏ {name},
+
+<b>ɪ ᴀᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ ᴀᴜᴛᴏ ᴀᴘᴘʀᴏᴠᴇ ʙᴏᴛ! 🚀</b>
+
+◈━━━━━━━━━━━━━━━━━━━◈
+✨ <b><u>ᴏᴜʀ ᴍᴀɪɴ ᴄʜᴀɴɴᴇʟꜱ</u></b>:
+📢 @Hindi_Tv_Verse
+📢 @AJ_TVSERIAL
+
+🛠 <b><u>ꜱᴜᴘᴘᴏʀᴛ</u></b>: @SerialVerse_support
+◈━━━━━━━━━━━━━━━━━━━◈
+"""
+
+# Image Links
+IMG_LINKS = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTJM3_zTxRsWGppcqvJL7qyxfkSD_M0eaYP7Urk_2a3A&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCnSHHm-CEQOWFsqpW05431l3-HwVz1SknjfKaNM6cOWVj42PhePG8ZPqL&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi5bHTZA1z_dzKCNcoNF1-UhzYCvJOQay4CbmGsnHdkmriTddaS1te3YnZ&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNPepjBEVhzH-w7i6eYRfCr076VzPaLR4MUMTQvZrtYg&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxPuz1FXFIhj5knSOdCQt1wbPhdNQWdx3ppycSXHdMXng0fcSWEbJKSA4&s=10",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9QeSYWssby38Xsg3K9CFCc01cCjmD582_ydQan6x8RA&s=10"
+]
